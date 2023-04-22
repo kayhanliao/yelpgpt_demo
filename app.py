1fca0496f2a3d3f196ef8932dc3606d7bd041006
Kayhan <42952515+kayhanliao@users.noreply.github.com>
2023-04-22 03:36:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -112,13 +112,14 @@ if not app.debug:
 
 # Default port:
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
+#     # Bind to PORT if defined, otherwise default to 5000.
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port)
 
-# Or specify port manually:
-'''
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+# # Or specify port manually:
+# '''
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port)
+# '''
